fd8cd1996bdd1b85b96ba91a94f5b23576607c06
Bethwel Kipkogei <bethwel.kiruok@vertexhubgroup.com>
2026-02-23 10:11:12 +0300

    retry

--- resources/views/layouts/kingsbridge.blade.php
+++ resources/views/layouts/kingsbridge.blade.php
@@ -27,7 +27,7 @@
     <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
   @endif
   <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ @filemtime(public_path('css/style.css')) }}">
-  <link rel="stylesheet" href="{{ asset('css/kingsbridge-modern.css') }}?v=20260222i">
+  <link rel="stylesheet" href="{{ asset('css/kingsbridge-modern.css') }}?v={{ @filemtime(public_path('css/kingsbridge-modern.css')) }}">
   <style>
     body.kb-modern{background:#f3f5f9!important;font-family:"Plus Jakarta Sans",sans-serif!important}
     .kb-nav-shell{position:sticky;top:0;z-index:1000;background:linear-gradient(90deg,#020617,#0f172a)!important}
